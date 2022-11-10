ff1fb2b85cbd1a796e7adf7d3d80e6fbc2ef86f6
HellOwhatAs <88815487+HellOwhatAs@users.noreply.github.com>
2022-11-10 20:25:47 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name="WhileDB",
+    python_requires='==3.8.*',
+    description="WhileDB language intepreter intrduced on SJTU CS2612.",
+    url="https://github.com/HellOwhatAs/While-DB",
+    version="0.0.1",
+    packages=['WhileDB'],
+    author="HellOwhatAs",
+    license="MIT",
+    include_package_data=True,
+    platforms=['windows'],
+    classifiers=[
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
